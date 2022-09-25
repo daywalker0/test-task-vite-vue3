60636f6af28c0a9dc4b82e0c6da67d4a95a51c9e
Maksim Arkhipov <Max1m.ar@yandex.ru>
2022-09-25 20:24:38 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viewDepthKey } from 'vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/test-task-vite-vue3/',
   plugins: [vue()]
 })
