03afa4be647f32ee6d13df1caf857dd982765554
Maksim Arkhipov <97803468+daywalker0@users.noreply.github.com>
2022-09-26 11:45:27 +0300

    Delete dist directory

--- dist/assets/About.6838581a.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.66439598.js";const o={};function r(e,n,s,c,_,a){return null}const f=t(o,[["render",r]]);export{f as default};
--- dist/assets/Home.e3f50140.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.66439598.js";const e={};function o(r,n,s,c,_,a){return null}const f=t(e,[["render",o]]);export{f as default};
